5e79521229a29b05e2d6b6f18dba6a078e4d39f3
kristtep <kristtep@stud.ntnu.no>
2022-03-01 12:05:12 +0100

    fullscreen on click

--- previs/src/components/SendStream.jsx
+++ previs/src/components/SendStream.jsx
@@ -1,47 +1,62 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import "../styles.css";
 import { Context } from "../socket";
 
+
+
 const SendStream = () => {
     
-    const [grid, setGrid] = useState(true);
     const { vid1, callAccepted, callEnded, incomingVoice } = useContext(Context);
-    const largeVideo = () => {
-        setGrid(!grid);
-    }
 
+    const toggleFullscreen = (num) => {
+        let elem = document.getElementById(num);
+        if(elem){
+
+          if(!document.fullscreenElement){
+            elem.requestFullscreen()
+            .then(() => {})
+            .catch(err => {
+              alert(`error on try fullscreen mode: ${err.message} (${err.name})`);
+            });
+          } else {
+            document.exitFullscreen();
+          }
+        }
+    }
 
     const track = (num) => {
 
       setTimeout(() => {
+
         let src = new MediaStream();
         src.addTrack(vid1.current.getTracks()[num]);
         let video = document.getElementById(`${num}`);
-        video.srcObject = src;
         
-      
+        video.srcObject = src;
+
       }, 10000);
 
     }
 
     
     return (
+
         <>
         <div id="stream">
-            {vid1 && (
+            
               <>
                 <div id="firstrow">
-                    <video id="1" onClick={largeVideo} width = {grid ? "600" : "1600"} height = {grid ? "400" : "900"} playsInline muted src={track(1)} autoPlay />
-                    <video id="2" onClick={largeVideo} width = {grid ? "600" : "1600"} height = {grid ? "400" : "900"} playsInline muted src={track(2)} autoPlay />
+                    <video id="1" onClick={() => {toggleFullscreen(1)}} width = "600" height = "400" playsInline muted src={track(1)} autoPlay />
+                    <video id="2" onClick={() => {toggleFullscreen(2)}} width = "600" height = "400" playsInline muted src={track(2)} autoPlay />
                 </div>
             
                 <div id="secondrow">
-                    <video id="3" width = "600" height = "400" playsInline muted src={track(3)} autoPlay />
-                    <video id="4" width = "600" height = "400" playsInline muted src={track(4)} autoPlay />
+                    <video id="3" onClick={() => {toggleFullscreen(3)}} width = "600" height = "400" playsInline muted src={track(3)} autoPlay />
+                    <video id="4" onClick={() => {toggleFullscreen(4)}} width = "600" height = "400" playsInline muted src={track(4)} autoPlay />
                 </div>
             
               </>
-            )}
+            
         </div>
         {callAccepted && !callEnded && (
           <div>
@@ -49,7 +64,7 @@ const SendStream = () => {
           </div>
         )}
         </>
-    )     
+    )
 
 };
 
--- previs/src/components/ViewStream.jsx
+++ previs/src/components/ViewStream.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import "../styles.css";
 import { Context } from "../socket";
 
@@ -6,11 +6,29 @@ const ViewStream = () => {
     
     const { startWatch, me, vid1, callEnded, callAccepted } = useContext(Context);
 
+    const toggleFullscreen = (num) => {
+        let elem = document.getElementById(`v${num}`);
+        if(elem){
+
+          if(!document.fullscreenElement){
+            elem.requestFullscreen()
+            .then(() => {})
+            .catch(err => {
+              alert(`error on try fullscreen mode: ${err.message} (${err.name})`);
+            });
+          } else {
+            document.exitFullscreen();
+          }
+        }
+    }
 
     const track = (num) => {
 
         setTimeout(() => {
           let src = new MediaStream();
+          if(num === 1){
+              src.addTrack(vid1.current.getTracks()[0]);
+          }
           src.addTrack(vid1.current.getTracks()[num]);
           let video = document.getElementById(`v${num}`);
           video.srcObject = src;
@@ -27,12 +45,12 @@ const ViewStream = () => {
             {startWatch && callAccepted && !callEnded && (
                 <div id="stream">   
                     <div id="firstrow">
-                        <video id="v1" width = "600" height = "400" playsInline src={track(1)} autoPlay />
-                        <video id="v2" width = "600" height = "400" playsInline src={track(2)} autoPlay />
+                        <video id="v1" onClick={() => {toggleFullscreen(1)}} width = "600" height = "400" playsInline src={track(1)} autoPlay />
+                        <video id="v2" onClick={() => {toggleFullscreen(2)}} width = "600" height = "400" playsInline src={track(2)} autoPlay />
                     </div>
                     <div id="secondrow">
-                        <video id="v3" width = "600" height = "400" playsInline src={track(3)} autoPlay />
-                        <video id="v4" width = "600" height = "400" playsInline src={track(4)} autoPlay />
+                        <video id="v3" onClick={() => {toggleFullscreen(3)}} width = "600" height = "400" playsInline src={track(3)} autoPlay />
+                        <video id="v4" onClick={() => {toggleFullscreen(4)}} width = "600" height = "400" playsInline src={track(4)} autoPlay />
                     </div>            
                 </div>
             )}
