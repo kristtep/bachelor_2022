231b5f805cc4eb60a013ea5d3625e5febe92ec41
kristtep <kristtep@stud.ntnu.no>
2022-03-24 13:58:50 +0100

    sharing screen from hospital

--- previs/src/components/SendStream.jsx
+++ previs/src/components/SendStream.jsx
@@ -4,11 +4,11 @@ import { Context } from "../socket";
 
 const SendStream = () => {
 
-    const { shareScreen, vid1, callAccepted, callEnded, incomingVoice, getCameras } = useContext(Context);
+    const { shareScreen, vid1, callAccepted, callEnded, incoming, getCameras } = useContext(Context);
 
     useEffect(() => {
       makeVideoElems();
-    }, [shareScreen]);
+    }, [shareScreen, callAccepted]);
 
     const toggleFullscreen = (num) => {
         let elem = document.getElementById(`${num}`);
@@ -32,7 +32,6 @@ const SendStream = () => {
           await getCameras();
         }
 
-
         let tracks = vid1.current.getVideoTracks();
 
         console.log(tracks);
@@ -49,29 +48,31 @@ const SendStream = () => {
 
             document.getElementById('stream').appendChild(elem);
             setSrc(i);
+            }else{
+              let del = document.getElementById(i);
+              del.remove();
             }
           }
         }
+      
     }
 
     const setSrc = (i) => {
 
       const tracks = vid1.current.getVideoTracks();
-
-        let src = new MediaStream();
-        src.addTrack(tracks[i-1]);
-        document.getElementById(`${i}`).srcObject = src;
-
+      let src = new MediaStream();
+      src.addTrack(tracks[i-1]);
+      document.getElementById(`${i}`).srcObject = src;
     }
 
     return (
         <>
-        <div id="stream"></div>
-
-        {callAccepted && !callEnded && (
-          <div>
-            <audio ref={incomingVoice} autoPlay />
+        {callAccepted && !callEnded ? (
+          <div id="mirror">
+            <video ref={incoming} autoPlay />
           </div>
+        ) : (
+          <div id="stream"></div>
         )}
         </>
     )
