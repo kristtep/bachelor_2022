6287ed467fa315e82dab9a8ec16848829eeb7519
Kristian Teppan <56825049+kristtep@users.noreply.github.com>
2022-05-12 21:09:52 +0200

    Update socket.js

--- previs/src/socket.js
+++ previs/src/socket.js
@@ -30,7 +30,7 @@ const ContextProvider = ({ children }) => {
     var isChannelReady = false;
     var isInitiator = false;
     var isStarted = false;
-    var clientName = "Ambulance " + Math.floor(Math.random() * 100 + 1);
+    var clientName = "Ambulanse " + Math.floor(Math.random() * 100 + 1);
     var remoteClient;
     var currentRoom;
     const [status, setStatus] = useState({ ready: false, room: '', ambulance: '' });
